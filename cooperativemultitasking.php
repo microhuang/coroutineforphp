5d01b3302146b68f36e0f0cd19e0fb29cabc7f1f
microhuang <microhuang@live.com>
2013-11-25 18:16:37 +0800

    Update cooperativemultitasking.php

--- cooperativemultitasking.php
+++ cooperativemultitasking.php
@@ -191,6 +191,45 @@ class Scheduler {
 }
 
 
+function stackedCoroutine(Generator $gen) {
+    $stack = new SplStack;
+
+    for (;;) {
+        $value = $gen->current();
+
+        if ($value instanceof Generator) {
+            $stack->push($gen);
+            $gen = $value;
+            continue;
+        }
+
+        $isReturnValue = $value instanceof CoroutineReturnValue;
+        if (!$gen->valid() || $isReturnValue) {
+            if ($stack->isEmpty()) {
+                return;
+            }
+
+            $gen = $stack->pop();
+            $gen->send($isReturnValue ? $value->getValue() : NULL);
+            continue;
+        }
+
+        $gen->send(yield $gen->key() => $value);
+    }
+}
+
+class CoroutineReturnValue {
+    protected $value;
+
+    public function __construct($value) {
+        $this->value = $value;
+    }
+
+    public function getValue() {
+        return $this->value;
+    }
+}
+
 
 function waitForRead($socket) {
     return new SystemCall(
